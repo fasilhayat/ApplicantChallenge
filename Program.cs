bbc1c5eed1409578683df48471275d140aa4e953
fasilhayat <55887303+fasilhayat@users.noreply.github.com>
2019-09-27 23:52:43 +0200

    Initial upload
    Applicant challenge v1.0

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Challenge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static List<int> result = new List<int>();
+        static int cursorPosition = 0;
+        static int index = 0;
+        static List<List<int>> pyramid = Pyramid.Inputdata();
+
+        static void Main(string[] args)
+        {
+            var res = CalculateMaxPath(cursorPosition, pyramid[index]);
+            Console.WriteLine(res.Sum());
+        }
+
+        static List<int> CalculateMaxPath(int cursorPosition, List<int> sequence)
+        {
+            var noi = new int[3];
+            noi[0] = sequence[cursorPosition];
+            if (index != pyramid.Count-1)
+            {
+                noi[1] = pyramid[index + 1][cursorPosition];
+                noi[2] = pyramid[index + 1][cursorPosition + 1];
+            }
+
+            if (noi[0] % 2 != noi[1] % 2 && noi[0] % 2 != noi[2] % 2 && noi[1] < noi[2] || noi[0] % 2 == noi[1] % 2 && noi[0] % 2 != noi[2] % 2) 
+            {
+                cursorPosition++;
+            }
+
+            result.Add(noi[0]);
+            index++;
+
+            return (index == pyramid.Count()) ? result : CalculateMaxPath(cursorPosition, pyramid[index]);
+        }
+    }
+}
---
+++ Pyramid.cs
@@ -0,0 +1,31 @@
+﻿namespace Challenge
+{
+    using System.Collections.Generic;
+
+    public static class Pyramid
+    {
+        public static List<List<int>> Inputdata()
+        {
+            List<List<int>> pyramidNumbers = new List<List<int>>
+            {
+                new List<int> { 215 },
+                new List<int> { 192, 124 },
+                new List<int> { 117, 269, 442 },
+                new List<int> { 218, 836, 347, 235 },
+                new List<int> { 320, 805, 522, 417, 345 },
+                new List<int> { 229, 601, 728, 835, 133, 124 },
+                new List<int> { 248, 202, 277, 433, 207, 263, 257 },
+                new List<int> { 359, 464, 504, 528, 516, 716, 871, 182 },
+                new List<int> { 461, 441, 426, 656, 863, 560, 380, 171, 923 },
+                new List<int> { 381, 348, 573, 533, 448, 632, 387, 176, 975, 449 },
+                new List<int> { 223, 711, 445, 645, 245, 543, 931, 532, 937, 541, 444 },
+                new List<int> { 330, 131, 333, 928, 376, 733, 017, 778, 839, 168, 197, 197 },
+                new List<int> { 131, 171, 522, 137, 217, 224, 291, 413, 528, 520, 227, 229, 928 },
+                new List<int> { 223, 626, 034, 683, 839, 052, 627, 310, 713, 999, 629, 817, 410, 121 },
+                new List<int> { 924, 622, 911, 233, 325, 139, 721, 218, 253, 223, 107, 233, 230, 124, 233 },
+            };
+
+            return pyramidNumbers;
+        }
+    }
+}
