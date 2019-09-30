f2b6ec69f4a63ffb44f48cd08a80aafde4be6151
fasilhayat <55887303+fasilhayat@users.noreply.github.com>
2019-09-30 12:53:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,41 +1,44 @@
-﻿namespace Challenge
+namespace Challenge
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
 
     class Program
-    {
-        static List<int> result = new List<int>();
-        static int cursorPosition = 0;
-        static int index = 0;
+    { 
         static List<List<int>> pyramid = Pyramid.Inputdata();
 
         static void Main(string[] args)
         {
-            var res = CalculateMaxPath(cursorPosition, pyramid[index]);
-            Console.WriteLine(res.Sum());
+            List<int> result = new List<int>();
+
+            CalculateMaxPath(0, 0, result);
+            Console.WriteLine(result.Sum());
+            Console.ReadKey();
         }
 
-        static List<int> CalculateMaxPath(int cursorPosition, List<int> sequence)
+        static void CalculateMaxPath(int row, int column, List<int> result)
         {
             var noi = new int[3];
-            noi[0] = sequence[cursorPosition];
-            if (index != pyramid.Count-1)
+            noi[0] = pyramid[row][column];
+            if (row != pyramid.Count - 1)
             {
-                noi[1] = pyramid[index + 1][cursorPosition];
-                noi[2] = pyramid[index + 1][cursorPosition + 1];
+                noi[1] = pyramid[row + 1][column];
+                noi[2] = pyramid[row + 1][column + 1];
             }
 
-            if (noi[0] % 2 != noi[1] % 2 && noi[0] % 2 != noi[2] % 2 && noi[1] < noi[2] || noi[0] % 2 == noi[1] % 2 && noi[0] % 2 != noi[2] % 2) 
+            if (noi[0] % 2 != noi[1] % 2 && noi[0] % 2 != noi[2] % 2 && noi[1] < noi[2] || noi[0] % 2 == noi[1] % 2 && noi[0] % 2 != noi[2] % 2)
             {
-                cursorPosition++;
-            }
+                column++;
+            } 
 
             result.Add(noi[0]);
-            index++;
+            row++;
 
-            return (index == pyramid.Count()) ? result : CalculateMaxPath(cursorPosition, pyramid[index]);
+            if (row != pyramid.Count())
+            {
+                CalculateMaxPath(row, column, result); 
+            }
         }
     }
 }
