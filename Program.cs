0fef65170cb54b61592af89825680da2bd4f865d
Fasil Malik Hayat <55887303+fasilhayat@users.noreply.github.com>
2025-03-30 10:46:05 +0200

    Update Program.cs
    Updated the applicant challenge with DP (dynamic programming) Bottom-Up traversal approach.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace Challenge
     using System.Collections.Generic;
     using System.Linq;
 
+    /* My old implementation in 20219
     class Program
     { 
         static List<List<int>> pyramid = Pyramid.Inputdata();
@@ -39,6 +40,68 @@ namespace Challenge
             {
                 CalculateMaxPath(row, column, result); 
             }
+        }
+    }*/
+    class Program
+    {
+        static List<List<int>> pyramid = Pyramid.Inputdata();
+    
+        static void Main(string[] args)
+        {
+            List<int> path = new List<int>();
+            int maxSum = CalculateMaxPath(out path);
+    
+            Console.WriteLine($"Maximum Path Sum: {maxSum}");
+            Console.WriteLine($"Path Taken: {string.Join(" -> ", path)}");
+            Console.ReadKey();
+        }
+    
+        static int CalculateMaxPath(out List<int> path)
+        {
+            int numRows = pyramid.Count;
+            int[][] dp = new int[numRows][];
+            int[][] pathIndex = new int[numRows][];
+    
+            for (int i = 0; i < numRows; i++)
+            {
+                dp[i] = new int[pyramid[i].Count];
+                pathIndex[i] = new int[pyramid[i].Count];
+            }
+    
+            // Copy last row values
+            for (int col = 0; col < pyramid[numRows - 1].Count; col++)
+            {
+                dp[numRows - 1][col] = pyramid[numRows - 1][col];
+            }
+    
+            // Bottom-up DP processing
+            for (int row = numRows - 2; row >= 0; row--)
+            {
+                for (int col = 0; col < pyramid[row].Count; col++)
+                {
+                    if (dp[row + 1][col] > dp[row + 1][col + 1])
+                    {
+                        dp[row][col] = pyramid[row][col] + dp[row + 1][col];
+                        pathIndex[row][col] = col;
+                    }
+                    else
+                    {
+                        dp[row][col] = pyramid[row][col] + dp[row + 1][col + 1];
+                        pathIndex[row][col] = col + 1;
+                    }
+                }
+            }
+    
+            // Reconstruct the path
+            path = new List<int>();
+            int colIndex = 0;
+            for (int row = 0; row < numRows; row++)
+            {
+                path.Add(pyramid[row][colIndex]);
+                colIndex = pathIndex[row][colIndex];
+            }
+    
+            return dp[0][0];
         }
     }
 }
